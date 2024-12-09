467618759d7165fc0c4ddac9a2b670e00aaf86d5
mnz8 <mnz8@foxmail.com>
2024-12-09 22:42:28 +0800

    feat: Automatic Batching

---
+++ prove_react_18_vite/src/pages/ReactBatchingClass.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+
+interface ReactBatchingClassState {
+  count: number;
+}
+
+export class ReactBatchingClass extends React.Component<
+  unknown,
+  ReactBatchingClassState
+> {
+  constructor(props: unknown) {
+    super(props);
+    this.state = {
+      count: 0,
+    };
+  }
+
+  componentDidMount() {
+    this.setState({ count: this.state.count + 1 });
+    console.log(this.state.count); // 0
+    this.setState({ count: this.state.count + 1 });
+    console.log(this.state.count); // 0
+
+    setTimeout(() => {
+      this.setState({ count: this.state.count + 1 });
+      console.log(this.state.count); // 1
+      this.setState({ count: this.state.count + 1 });
+      console.log(this.state.count); // 1
+    }, 0);
+  }
+
+  render() {
+    return <>ReactBatchingClass</>;
+  }
+}
